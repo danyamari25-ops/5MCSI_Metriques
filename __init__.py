b62f0636f4033090c589523db5bb159163e536b6
danyamari25-ops <danyamari25@gmail.com>
2025-12-12 10:07:51 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,7 +10,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
-  @app.route("/contact/")
+
+
+@app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
   
